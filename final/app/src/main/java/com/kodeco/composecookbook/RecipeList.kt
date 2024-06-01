ddf07c48ffa769c9d51ab53cc3fccb8b6f525975
noufel-boulkroune <noufelboulkroune@gmail.com>
2024-06-01 23:06:59 +0100

    first commit

---
+++ final/app/src/main/java/com/kodeco/composecookbook/RecipeList.kt
@@ -0,0 +1,21 @@
+package com.kodeco.composecookbook
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+
+@Composable
+fun RecipeList(viewModel: RecipeListViewModel) {
+    // Convert the flow (of MutableStateList) into a State
+    val recipeListState = viewModel.recipeListFlow.collectAsStateWithLifecycle()
+
+    LazyColumn {
+        items(recipeListState.value) {
+            RecipeCard(it, Modifier.padding(16.dp)) // Added a modifier argument here
+        }
+    }
+}
---
+++ intermediate/app/src/main/java/com/kodeco/composecookbook/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.kodeco.composecookbook
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import com.kodeco.composecookbook.ui.theme.ComposeCookbookTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCookbookTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Cookbook()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Cookbook() {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Compose Cookbook",
+                    modifier = Modifier.testTag("topAppBarText")
+                )
+            },
+            colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.LightGray),
+            modifier = Modifier.testTag("topAppBar")
+        )
+        RecipeList(recipes = defaultRecipes)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CookbookPreview() {
+    ComposeCookbookTheme {
+        Cookbook()
+    }
+}
